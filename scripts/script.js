96d55fb791447fa1f722dcfb00496e592c644d33
insect2510 <155474669+insect2510@users.noreply.github.com>
2026-03-12 00:31:53 +0100

    Commit to new Branch sidemenu

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+function openNav() {
+  document.getElementById("navigation-top").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("navigation-top").style.width = "0";
+}
