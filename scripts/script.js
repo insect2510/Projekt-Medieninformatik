2a14e19409181d146fa4b9d20f9a1e1414a17b11
insect2510 <155474669+insect2510@users.noreply.github.com>
2026-03-12 01:38:31 +0100

    Update Hero und Sidebar

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 function openNav() {
-  document.getElementById("navigation-top").style.width = "100%";
+  document.getElementById("navigation-top").classList.add("menu-open");
 }
 
 function closeNav() {
-  document.getElementById("navigation-top").style.width = "0";
+  document.getElementById("navigation-top").classList.remove("menu-open");
 }
